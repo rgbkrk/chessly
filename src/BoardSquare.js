e066be6aceb46c86b9f5c0171486b340906f8598
Kyle Kelley <rgbkrk@gmail.com>
2016-01-09 01:47:26 -0600

    Render an overlay for placement.

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -5,44 +5,53 @@ import { ItemTypes } from './Constants';
 import { DropTarget } from 'react-dnd';
 
 const squareTarget = {
+  canDrop(props) {
+    return canMoveKnight(props.x, props.y);
+  },
   drop(props, monitor) {
     moveKnight(props.x, props.y);
   }
 }
 
+
 function collect(connect, monitor) {
   return {
     connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
   }
 }
 
 class BoardSquare extends Component {
+  renderOverlay(color) {
+    return (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+    );
+  }
+
   render() {
-    const { x, y, connectDropTarget, isOver } = this.props;
+    const { x, y, connectDropTarget, isOver, canDrop } = this.props;
     const black = (x + y) % 2 === 1;
 
     return connectDropTarget(
       <div style={{
         position: 'relative',
-        width: '100%',
-        height: '100%'
       }}>
         <Square black={black}>
           {this.props.children}
         </Square>
-        {isOver &&
-          <div style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100%',
-            width: '100%',
-            zIndex: 1,
-            opacity: 0.5,
-            backgroundColor: 'yellow',
-          }} />
-        }
+        {isOver && !canDrop && this.renderOverlay('red')}
+        {!isOver && canDrop && this.renderOverlay('yellow')}
+        {isOver && canDrop && this.renderOverlay('green')}
       </div>
     )
   }
@@ -51,7 +60,8 @@ class BoardSquare extends Component {
 BoardSquare.propTypes = {
   x: PropTypes.number.isRequired,
   y: PropTypes.number.isRequired,
-  isOver: PropTypes.bool.isRequired
+  isOver: PropTypes.bool.isRequired,
+  canDrop: PropTypes.bool.isRequired
 };
 
 
