028b49dd85a6f0c50875b509851d26e1658e55db
Kyle Kelley <rgbkrk@gmail.com>
2016-01-09 00:26:12 -0600

    Observe some fake data.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Board from './Board';
+import { observe } from './Game';
 
-ReactDOM.render(
-  <Board knightPosition={[0, 0]} />,
-  document.getElementById('root')
+observe(knightPosition =>
+  ReactDOM.render(
+    <Board knightPosition={knightPosition} />,
+    document.getElementById('root')
+  )
 );
