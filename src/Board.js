248f3ab9c418180c9ca24bf34b4eb344fc77b028
Kyle Kelley <rgbkrk@gmail.com>
2016-01-09 01:41:41 -0600

    Make our drop target.

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import Square from './Square';
+import BoardSquare from './BoardSquare';
 import Knight from './Knight';
 import { moveKnight, canMoveKnight } from './Game';
 
@@ -12,28 +12,20 @@ class Board extends Component {
     const x = i % 8;
     const y = Math.floor(i / 8);
 
-    const black = (x + y) % 2 === 1;
-
-    const [knightX, knightY] = this.props.knightPosition;
-    const piece = (x === knightX && y === knightY) ?
-      <Knight /> :
-      null;
-
     return (
       <div key={i}
-           style={{ width: '12.5%', height: '12.5%' }}
-           onClick={() => this.handleSquareClick(x, y)}
-           >
-        <Square black={black}>
-          {piece}
-        </Square>
+           style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x} y={y}>
+          {this.renderPiece(x,y)}
+        </BoardSquare>
       </div>
     )
   }
 
-  handleSquareClick(toX, toY) {
-    if (canMoveKnight(toX, toY)) {
-      moveKnight(toX, toY);
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />;
     }
   }
 
